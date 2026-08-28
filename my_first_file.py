7d69ac78084f4e04e1bca498ff7cbbc8c3686b1c
navalesupriya001-cell <navalesupriya001@gmail.com>
2026-08-28 22:40:33 +0530

    This is my first commit - woohoo

---
+++ my_first_file.py
@@ -0,0 +1,3 @@
+print("This is a new file and this is my first commit")
+print("I am learning how to use Git and GitHub for version control.")
+print("This is a simple Python script to demonstrate basic Git operations.")
