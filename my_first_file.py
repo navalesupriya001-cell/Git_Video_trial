e4488dfb9e6e86906c5eb87fa7b0b03a7a34d457
navalesupriya001-cell <navalesupriya001@gmail.com>
2026-08-28 22:57:27 +0530

    Second Commit w updated file

--- my_first_file.py
+++ my_first_file.py
@@ -1,3 +1,7 @@
-print("This is a new file and this is my first commit")
-print("I am learning how to use Git and GitHub for version control.")
-print("This is a simple Python script to demonstrate basic Git operations.")
+# "This is a new file and this is my first commit"
+# "I am learning how to use Git and GitHub for version control."
+# "This is a simple Python script to demonstrate basic Git operations."
+
+# this is our code
+
+print("I love Git and GitHub")
